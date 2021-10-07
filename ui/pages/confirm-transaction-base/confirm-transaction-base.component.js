09f3527be1b9520cfe348c53487b21ba581c7e51
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-10-07 19:53:16 +0100

    Show a different error message when there's a transaction error     Show something went wrong     Show More Details.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -148,7 +148,7 @@ export default class ConfirmTransactionBase extends Component {
     isMultiLayerFeeNetwork: PropTypes.bool,
     isFailedTransaction: PropTypes.bool,
     removeTxFromFailedTxesToDisplay: PropTypes.bool,
-    addTxToFailedTxesToDisplay: PropTypes.bool,
+    addTxToDisplay: PropTypes.bool,
   };
 
   state = {
@@ -808,7 +808,7 @@ export default class ConfirmTransactionBase extends Component {
       maxFeePerGas,
       maxPriorityFeePerGas,
       baseFeePerGas,
-      addTxToFailedTxesToDisplay,
+      addTxToDisplay,
     } = this.props;
     const { submitting } = this.state;
 
@@ -842,6 +842,7 @@ export default class ConfirmTransactionBase extends Component {
       () => {
         this._removeBeforeUnload();
 
+        addTxToDisplay(txData.id);
         sendTransaction(txData)
           .then(() => {
             clearConfirmTransaction();
@@ -856,7 +857,6 @@ export default class ConfirmTransactionBase extends Component {
             );
           })
           .catch((error) => {
-            addTxToFailedTxesToDisplay(txData.id);
             this.setState({
               submitting: false,
               submitError: error.message,
