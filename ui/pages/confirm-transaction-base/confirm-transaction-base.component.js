1c94b83197e7106145a379ec366f8625d2f2bd8a
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-10-25 19:27:26 +0400

    remove tx from failed display if call is successful
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -148,7 +148,7 @@ export default class ConfirmTransactionBase extends Component {
     isMultiLayerFeeNetwork: PropTypes.bool,
     isFailedTransaction: PropTypes.bool,
     removeTxFromFailedTxesToDisplay: PropTypes.func,
-    addTxToDisplay: PropTypes.func,
+    addTxToFailedTxesToDisplay: PropTypes.func,
   };
 
   state = {
@@ -808,7 +808,7 @@ export default class ConfirmTransactionBase extends Component {
       maxFeePerGas,
       maxPriorityFeePerGas,
       baseFeePerGas,
-      addTxToDisplay,
+      addTxToFailedTxesToDisplay,
     } = this.props;
     const { submitting } = this.state;
 
@@ -842,10 +842,11 @@ export default class ConfirmTransactionBase extends Component {
       () => {
         this._removeBeforeUnload();
 
-        addTxToDisplay(txData.id);
+        addTxToFailedTxesToDisplay(txData.id);
         sendTransaction(txData)
           .then(() => {
-            clearConfirmTransaction();
+            removeTxFromFailedTxesToDisplay();
+            clearConfirmTransaction();            
             this.setState(
               {
                 submitting: false,
